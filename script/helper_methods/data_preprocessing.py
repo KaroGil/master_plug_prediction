367f39d7fad8a920ec1fb09b1fde9a54cd484ad3
KaroGil <karolinkagil@gmail.com>
2026-02-04 11:14:23 +0100

    add plug index feature, fix some issues

--- script/helper_methods/data_preprocessing.py
+++ script/helper_methods/data_preprocessing.py
@@ -56,38 +56,21 @@ def create_target_column(df, flow_thresh=0.9, pressure_thresh=1.3, thresholds=['
         df["Plug"] = np.where((pressure_plug), 1, df["Plug"])
 
 
-def create_future_target(df, shift=-200):
+def create_future_target(df, horizon=200):
     '''Create target column 'Plug_future' by shifting 'Plug' column'''
 
-    # Predict plug event 10 steps ahead
-    df['Plug_future'] = df['Plug'].shift(shift)
+    df['Plug_future'] = (df['Plug'].shift(-1).rolling(window=horizon, min_periods=1).max())
     df.dropna(subset=['Plug_future'], inplace=True)
 
 
-def add_logId_column(df, log_id):
-    '''Add a LogId column to the dataframe to specify which log the data comes from'''
-    df = df.copy()
-    df['LogId'] = int(log_id[4:])
-    return df
-
-
-def split_data(X,y,test_size=0.01, test_logid=False):
+def split_data(X,y):
     '''Split data into train, validation, and test sets without shuffling'''
-    if test_logid: # instead of normal splitting, leave out one dataset
-        X_test = X.loc[X['LogId'] == X['LogId'].max()]
-        y_test = y.loc[y.index.isin(X_test.index)]
-        X_train = X.loc[X['LogId'] != X['LogId'].max()]
-        y_train = y.loc[y.index.isin(X_train.index)]
-    else:
-        n = len(X)
-        test_start_idx = int((1 - test_size) * n)
-        val_start_idx = int((1 - 2 * test_size) * n)
-
-        X_train = X.iloc[:val_start_idx]
-        y_train = y.iloc[:val_start_idx]
-
-        X_test = X.iloc[test_start_idx:]
-        y_test = y.iloc[test_start_idx:]
+    
+    X_test = X.loc[X['LogId'] == X['LogId'].max()]
+    y_test = y.loc[y.index.isin(X_test.index)]
+    
+    X_train = X.loc[X['LogId'] != X['LogId'].max()]
+    y_train = y.loc[y.index.isin(X_train.index)]
 
     return X_train, X_test, y_train, y_test
 
@@ -135,14 +118,19 @@ def align_features(df, FEATURES):
     return df
 
 
-def feature_engineering_windowing(df, log_id):
+def feature_engineering_windowing(df, dataset_name="data1"):
     '''Basic preprocessing pipeline without train/test split'''
-    df = add_logId_column(df, log_id)
+
+    if 'LogId' not in df.columns:
+        df['LogId'] = dataset_name[4:]
+
+    df = fe.feature_engineering_pipeline(df) 
 
     print("Adding time derivative features...")
     df = fe.add_time_derivative_features(df)
 
-    df = fe.feature_engineering_pipeline(df) 
+    print("Adding plug index feature...")
+    df = fe.plug_index(df)
 
     df_feat = df.select_dtypes(include=['number']).copy()
 
@@ -150,44 +138,38 @@ def feature_engineering_windowing(df, log_id):
 
     return X, y
 
-def preprocess_data(df, dataset_name, additional_data = None, additional_data_name = None, BASE_PATH = ""):
+
+def preprocess_data(datasets, dataset_names, BASE_PATH = ""):
     '''Full preprocessing pipeline for model selection and training'''
 
-    X, y = feature_engineering_windowing(df, dataset_name)
-    if additional_data is not None and additional_data_name is not None:
-        for add_df, add_name in zip(additional_data, additional_data_name):
-            print(f"Processing additional data: {add_name}") 
-            X_add, y_add = feature_engineering_windowing(add_df, add_name)
-            X = pd.concat([X, X_add], ignore_index=True)
-            y = pd.concat([y, y_add], ignore_index=True)
+    print(f"Processing data: {dataset_names[0]}")
+    X, y = feature_engineering_windowing(datasets[0], dataset_names[0])
 
-    X_train, X_test, y_train, y_test = split_data(X, y, test_logid=True) # Splits data into X and y, then into train/test sets. Removes Plug and Plug_future from X
+    for df, name in zip(datasets[1:], dataset_names[1:]):
+        print(f"Processing data: {name}") 
+        X_add, y_add = feature_engineering_windowing(df, name)
+
+        X = pd.concat([X, X_add], ignore_index=True)
+        y = pd.concat([y, y_add], ignore_index=True)
+    print(X.head(5))
+    print("Nan values in data", X.isna().sum())
+    X_train, X_test, y_train, y_test = split_data(X, y)
     print_distribution(y_train, "Training set")
     print_distribution(y_test, "Test set")
 
+    # Feature reduction
     X_train, selected = fr.shap_feature_importance(X_train, y_train, shap_subset_size=50)
     X_test = fr.remove_shap_low_importance_features(X_test, selected)
 
     X_train, to_drop = fr.remove_correlated_features(X_train, threshold=0.9)
     X_test = X_test.drop(columns=to_drop)
     
+    print("Types", X_train.dtypes)
     #drop any remaining NaN values
     X_train.fillna(0,inplace=True)
     X_test.fillna(0,inplace=True) 
     y_train = y_train.loc[X_train.index]
     y_test = y_test.loc[X_test.index]
-
-    X_train, X_test = scale_features(X_train, X_test)
-
-    # print("Visualizing before augmentation")
-    # from . import data_visualization as dv
-    # all = X_train.copy()
-    # all['Plug_future'] = y_train
-    # dv.visualize_plug_event(all, name="Training Set Before Augmentation")
-    # X_train, y_train = fe.augment_minority_continuous_timeseries(X_train, y_train)
-    # all_after = X_train.copy()
-    # all_after['Plug_future'] = y_train
-    # dv.visualize_plug_event(all_after, name="Training Set After Augmentation")
     
     data_to_save = {
         'X_train': X_train,
@@ -196,6 +178,9 @@ def preprocess_data(df, dataset_name, additional_data = None, additional_data_na
         'y_test': y_test
     }
     basePath = BASE_PATH + "data/processed_data/"
+    print("DatasetNames", dataset_names)
+    dataset_name = "data_" + "_".join(name[4:] for name in dataset_names)
+
     dl.save_data(data_to_save, dataset_name, base_path=basePath)
 
     joblib.dump(X_train.columns.tolist(), FEATURES_PATH)
@@ -212,13 +197,8 @@ def preprocess_data_predict(df, dataset_name):
 
     X = align_features(X, FEATURES)
 
-    scalar = joblib.load(scaler_path)
-
-    numeric_cols = X.select_dtypes(include=['number']).columns.tolist()
-    numeric_cols = [col for col in numeric_cols if col not in ['Plug', 'Plug_future', 'Anomaly', 'LogId']]
-    unscaled_X = X.copy()
-    X[numeric_cols] = scalar.transform(X[numeric_cols])
-
     X = X.drop(columns=['LogId'])
 
-    return X, y, unscaled_X
+    print("Final columns for prediction:",("\n").join(X.columns.tolist()))
+
+    return X, y
